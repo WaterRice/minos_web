f3a011a93515b5639d10b42bb8d8fa35e7b3cfa2
lq <2534543027@qq.com>
2019-03-29 20:26:02 +0800

    complete FAQ page

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -5,29 +5,29 @@
       <v-layout justify-center>
         <v-flex>
           <v-timeline>
-            <v-timeline-item color="red lighten-2" large>
+            <v-timeline-item color="deep-purple lighten-4" large>
               <template v-slot:opposite>
                 <span>可以选用其他的编程语言吗?</span>
               </template>
-              <v-card class="elevation-2">
+              <v-card class="elevation-2" color="deep-purple lighten-4">
                 <v-card-title class="headline">目前可用语言</v-card-title>
                 <v-card-text>由于技术实力原因,目前仅支持JAVA语言,后续慢慢增加对其他主流语言的支持,(MINOS小组表示已菜哭</v-card-text>
               </v-card>
             </v-timeline-item>
-            <v-timeline-item color="red lighten-2" large>
+            <v-timeline-item color="pink lighten-4" large>
               <template v-slot:opposite>
                 <span>后台JDK版本是多少?</span>
               </template>
-              <v-card class="elevation-2">
+              <v-card class="elevation-2" color="pink lighten-4">
                 <v-card-title class="headline">后端JDK环境</v-card-title>
                 <v-card-text>当前是oracle jdk-8u12,这意味着你只可以写不高于这个版本的java程序</v-card-text>
               </v-card>
             </v-timeline-item>
-            <v-timeline-item color="red lighten-2" large>
+            <v-timeline-item color="lime lighten-3" large>
               <template v-slot:opposite>
                 <span>我该怎么组织java代码呢?</span>
               </template>
-              <v-card class="elevation-2">
+              <v-card class="elevation-2" color="lime lighten-3">
                 <v-card-title class="headline">代码模板</v-card-title>
                 <v-card-text>
                   <code class="java">
@@ -40,15 +40,19 @@
                 </v-card-text>
               </v-card>
             </v-timeline-item>
-            <v-timeline-item color="red lighten-2" large>
+            <v-timeline-item color="blue-grey lighten-3" large>
               <template v-slot:opposite>
                 <span>发现bug怎么办?</span>
               </template>
-              <v-card class="elevation-2">
+              <v-card class="elevation-2" color="blue-grey lighten-3">
                 <v-card-title class="headline">bug提交</v-card-title>
                 <v-card-text>
                   作者: 2534543027@qq.com,或者在
-                  <a src>这里</a>提issue.
+                  <v-btn
+                    flat
+                    href="https://github.com/WaterRice/minos_web/issues"
+                    color="primary"
+                  >这里</v-btn>提交issue.
                 </v-card-text>
               </v-card>
             </v-timeline-item>
